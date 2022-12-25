8d428b3d5889dae48127b3e4b9819b0f5dfb23bb
Andrew Konoplastiy <konoplastiy@gmail.cim>
2022-12-26 00:32:47 +0200

    Create the project

---
+++ src/test/java/com/workshop/andrew/AuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.andrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
